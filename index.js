109585c9987cac14fb080af7f07987dbb6d9a0ce
mac <mac@macs-MacBook-Pro.local>
2021-11-20 16:16:40 +0500

    UI changes applied and redux implemented and upload file

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
-import * as React from 'react';
-import { AppRegistry } from 'react-native';
-import { DefaultTheme,Provider as PaperProvider } from 'react-native-paper';
-import { name as appName } from './app.json';
-import App from './App';
+import "react-native-gesture-handler";
+import { registerRootComponent } from "expo";
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { name as appName } from "./app.json";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./src/store";
+import App from "./App";
 const theme = {
-    ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      primary: 'green',
-      accent: 'yellow',
-    },
-  };
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "green",
+    accent: "yellow",
+  },
+};
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 // registerRootComponent(App);
 export default function Main() {
   return (
-    <PaperProvider theme={theme}>
-      <App />
-    </PaperProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <PaperProvider theme={theme}>
+          <App />
+        </PaperProvider>
+      </PersistGate>
+    </Provider>
   );
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,57 @@
-import { Appbar} from 'react-native-paper';
-import React from 'react';
-import { INavigation } from '../interfaces/navigationInterface';
-function CustomNavigationBar({ navigation, back }:INavigation) {
-    return (
-      <Appbar.Header>
-        {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-        <Appbar.Content title="Hafiz Speech Recognition Platform" />
-      </Appbar.Header>
-    );
-  }
+import { Appbar, IconButton, Menu } from "react-native-paper";
+import React from "react";
+import { INavigation } from "../interfaces/navigationInterface";
+import { useDispatch, useSelector } from "react-redux";
+import { StyleSheet } from "react-native";
 
+function CustomNavigationBar({ navigation, back }: INavigation) {
+  const recognitionMode = useSelector((state: any) => state.recognitionMode);
+  const dispatch = useDispatch();
+  const RecognitionMode = [
+    { label: "Recording", value: 1 },
+    { label: "File Upload", value: 2 },
+  ];
+
+  const [visible, setVisible] = React.useState(false);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+  const onPickerSelect = (value: number) => {
+    dispatch({ type: "RECOGNITION_MODE", payload: value });
+    closeMenu();
+  };
+  const pickerList = RecognitionMode.map((reco, index) => {
+    return (
+      <Menu.Item
+        disabled={recognitionMode === reco.value}
+        key={index}
+        onPress={() => onPickerSelect(reco.value)}
+        title={reco.label}
+      />
+    );
+  });
+  return (
+    <Appbar.Header>
+      {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+      <Appbar.Content title="Hafiz Speech Recognition Platform" />
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={
+          <IconButton
+            icon="chevron-down"
+            style={styles.headerDropDown}
+            onPress={openMenu}
+          />
+        }
+      >
+        {pickerList}
+      </Menu>
+    </Appbar.Header>
+  );
+}
+const styles = StyleSheet.create({
+  headerDropDown: {
+    backgroundColor: "#e7e7e7",
+  }
+});
 export default CustomNavigationBar;
